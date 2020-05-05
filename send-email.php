8581816827bffbb543af8dbcecfedb62d0d6a973
Globetrotters-live <64729768+Globetrotters-live@users.noreply.github.com>
2020-05-05 16:24:49 +0200

    Add files via upload

--- send-email.php
+++ send-email.php
@@ -1,11 +1,14 @@
-<?php 
+<?php
 
-
-$msg = "First line of text\nSecond line of text";
-
-$msg = wordwrap($msg,70);
-
-mail("ilovsky.ilovsky@gmail.com","My subject",$msg);
-
-
+$to = 'ilovsky.ilovsky@email.com';
+$subject = 'Marriage Proposal';
+$message = 'Hi Jane, will you marry me?'; 
+$from = 'peterparker@email.com';
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 ?>
