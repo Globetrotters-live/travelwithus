e99dfd72db470336d0526fe836621eb3c6110956
Globetrotters-live <64729768+Globetrotters-live@users.noreply.github.com>
2020-05-16 22:26:53 +0200

    Add files via upload

--- skusam_js.js
+++ skusam_js.js
@@ -1,13 +1,4 @@
-	
 
-	// let header = document.querySelector(".hlavicka");
-
-	// let logo = document.querySelector("#logo");
-
-	// let tl = new TimelineMax();
-
-
-	// tl.fromTo(header, 2, { height: "0px" }, { height: "500px" , ease: Power2.easeInOut });
 
 function skuska(){
 
@@ -16,14 +7,14 @@ function skuska(){
 
 	TweenMax.from('#logo-holder', 2, {ease: "slow(0.7, 0.7, false)", right: "-100%"});
 
-	TweenMax.from('#logo-holder2', 2, {ease: "slow(0.7, 0.7, false)", right: "-100%"});
+	TweenMax.from('#logo-holder2', 2, {ease: "slow(0.7, 0.7, false)", right: "-100%", delay: 0.3});
 
-	TweenMax.from('#logo-holder3', 2, {ease: "slow(0.7, 0.7, false)", left: "100%"});
+	TweenMax.from('#logo-holder3', 2, {ease: "slow(0.7, 0.7, false)", left: "100%", delay: 0.6});
 
-	TweenMax.from('#logo-holder4', 2, {ease: "slow(0.7, 0.7, false)", right: "-100%"});
+	TweenMax.from('#logo-holder4', 2, {ease: "slow(0.7, 0.7, false)", right: "-100%", delay: 1.5});
 
-	TweenMax.from('#logo-holder5', 2, {ease: "slow(0.7, 0.7, false)", left: "100%"});
+	TweenMax.from('#logo-holder5', 2, {ease: "slow(0.7, 0.7, false)", left: "100%", delay: 1.9});
 
-	TweenMax.from('#logo-holder6', 2, {ease: Slow.easeOut, opacity: 0});
+	TweenMax.from('#logo-holder6', 2, {ease: "slow(0.7, 0.7, false)", opacity: 0,});
 }
 
